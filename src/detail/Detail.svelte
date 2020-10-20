e69eafec2c690f333d9d5906e896ba774132f5d6
Richard Tzanov <richard.tzanov@sainsburys.co.uk>
2020-10-20 12:58:26 +0100

    files

---
+++ src/detail/Detail.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { httpGet } from "../common/api";
+  import type { Book } from "../types";
+  import BackButtonRow from "../common/BackButtonRow.svelte";
+  import BookCover from "../common/BookCover.svelte";
+  import Button from "../common/Button.svelte";
+  import Header from "../common/Header.svelte";
+  export let id;
+
+  let book: Book;
+  onMount(async () => {
+    const { data } = await httpGet(`/${id}`);
+    book = data;
+  });
+</script>
+
+<style>
+  .detail {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(40vw, 20rem));
+    grid-template-rows: minmax(64vw, 32rem) auto;
+    gap: var(--spacingXLarge);
+  }
+  .cover {
+    position: relative;
+    display: flex;
+    margin-bottom: var(--spacingXLarge);
+  }
+  .favorite {
+    position: absolute;
+    width: 90%;
+    left: calc(10% + var(--spacingSmall));
+    bottom: var(--spacingLarge);
+  }
+</style>
+
+<BackButtonRow />
+
+<Header element="h1" size="large">Discover</Header>
+
+{#if book}
+  <div class="detail">
+    <div class="cover">
+      <BookCover {book} />
+      <div class="favorite">
+        <Button>{book.favorite ? 'Unfavorite' : 'Favorite'}</Button>
+      </div>
+    </div>
+    <div>
+      <Header>About</Header>
+      <p>{book.about}</p>
+    </div>
+  </div>
+{:else}Loading...{/if}
